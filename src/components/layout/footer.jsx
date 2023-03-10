ffa493ac53449f5b09cf5c58d0c3c0a555c18a25
hoangeel <hoang291198@gmail.com>
2023-03-10 17:10:26 +0700

    first commit

--- src/components/layout/footer.jsx
+++ src/components/layout/footer.jsx
@@ -10,11 +10,11 @@ import logoBg from "../../assets/img/FadedLogo.png"
 export default function Footer() {
   const router = useRouter()
   return (
-    <div className="w-full h-full bg-[#FFFFFF] relative">
-      <div className="w-full h-full bg-[#DAEDF8] mt-[283px] mb-[342px] max-lg:mb-[67px] max-lg:mt-[202px]">
-        <div className=' pt-[53px] pb-[78px] max-lg:px-[30px] max-xl:px-[50px] max-2xl:px-[100px] container px-[154px] mx-auto max-w-[1728px]'>
-          <h2 className='max-lg:text-[40px] max-lg:max-w-[221px] max-w-[350px] mx-auto'>GET IN TOUCH</h2>
-          <div className='max-lg:mt-[70px] max-lg:mb-[50px] max-lg:flex-col flex flex-row justify-center items-center mb-[62px] mt-[78px] hover:cursor-pointer hover:opacity-70'>
+    <div className="w-full h-full bg-[#FFFFFF] pt-[283px] max-lg:pt-[202px] relative">
+      <div className="w-full h-full bg-[#DAEDF8]  mb-[342px] max-lg:mb-[67px]">
+        <div className='max-sm:pt-[11px] pt-[53px] pb-[78px] max-lg:px-[30px] max-xl:px-[50px] max-2xl:px-[100px] container px-[154px] mx-auto max-w-[1728px]'>
+          <h2 className='max-lg:text-[40px] max-lg:leading-[48px] max-lg:max-w-[221px] max-w-[350px] mx-auto'>GET IN TOUCH</h2>
+          <div className='max-lg:mt-[90px] max-lg:mb-[60px] max-lg:flex-col flex flex-row justify-center items-center mb-[62px] mt-[10px] hover:cursor-pointer hover:opacity-70'>
             <div className='max-lg:w-[35px] max-lg:h-[34px] max-lg:m-0 bg-[#06269A] rounded-[50%] w-[102px] h-[99px] flex justify-center items-center mr-[37px] '><Image src={icon1} alt='' className='max-lg:h-[14px] max-lg:w-[17px]'/></div>
             <p className=' max-lg:text-[18px] max-xl:text-[40px] max-2xl:mr-0 mr-[136px] font-[600px] leading-[61px] text-[#0D2373] text-[50px]' >Beyondhomedisability@gmail.com</p>
           </div>
@@ -24,7 +24,7 @@ export default function Footer() {
           </div>
         </div>
       </div>
-      <Image src={logoBg} alt='' className='absolute top-[-155px] right-0 max-sm:top-[-100px]'/>
+      <Image src={logoBg} alt='' className='absolute top-[130px] right-0 max-sm:top-[48px] max-sm:w-[519px] h-auto'/>
       <div className='max-sm:py-[10px] max-sm:px-[30px] px-[104px] pt-[39px] pb-[23px] bg-[#0C2251] '><Image src={logo} alt='' className='hover:opacity-70 hover:cursor-pointer max-lg:h-[28px] w-auto'/></div>
     </div>
   )
